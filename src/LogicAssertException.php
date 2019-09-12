29f9b29f71555a094c9e708d05ab3630485465cd
tioncico <1067197739@qq.com>
2019-09-12 10:57:17 +0800

    commit

---
+++ src/LogicAssertException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tioncico
+ * Date: 2019/9/12 0012
+ * Time: 10:05
+ */
+
+namespace LogicAssert;
+
+
+class LogicAssertException extends \Exception
+{
+
+}
